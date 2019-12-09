6fbdd74ef1c1378c869066bd143f6c8ef66a48ad
mikeagamcolum <55404593+mikeagamcolum@users.noreply.github.com>
2019-12-08 22:32:18 -0600

    Add files via upload

--- LegendofLinq/LegendofLinq/Program.cs
+++ LegendofLinq/LegendofLinq/Program.cs
@@ -2,54 +2,41 @@
 using System.Linq;
 using System.Collections.Generic;
 using System.IO;
+using Gma.DataStructures.StringSearch;
 
 namespace LegendofLinq
 {
     class Program
     {
+        public static UkkonenTrie<Delegate> tree;
         static public List<String> getData()
         {
             return File.ReadAllLines("words.txt").ToList();
         }
-        static public List<String> ReverseSort()
-        {
-            List<String> data = getData();
-            data.Reverse();
-            Console.WriteLine("The list has been reversed.");
-            return data;
-        }
+
         static public List<String> StartingWith(String x)
         {
-            List<String> data = getData();
             List<String> results = new List<String>();
-            for(int c = 0; c < data.Count; c++)
-            {
-                if (data[c].StartsWith(x))
-                {
-                    results.Add(data[c]);
-                }
-            }
             return results;
         }
+
         static public List<String> NeedleHaystack(String n, int pos)
         {
-            List<String> data = getData();
             List<String> results = new List<String>();
-            for (int c = 0; c < data.Count; c++)
-            {
-                if (data[c].Length > 2)
-                {
-                    if (data[c].Substring(pos, n.Length) == n) ;
-                    {
-                        results.Add(data[c]);
-                    }
-                }
-            }
             return results;
         }
+
+
+
         static void Main(string[] args)
         {
-            ReverseSort();
+            tree = new UkkonenTrie<Delegate>(getData().Count);    
+            for (int x = 0; x < getData().Count - 1; x++)
+            {
+            //    delegate var temp = tree.Retrieve(getData()[x + 1]);
+            //    tree.Add(getData()[x], temp);
+            }
+
             Console.WriteLine(StartingWith("Z").Count + " words start with 'Z'.");
             Console.WriteLine(StartingWith("He").Count + " words start with \"He\".");
             Console.WriteLine(NeedleHaystack("e", 1).Count + " words have 'e' in the second position.");
